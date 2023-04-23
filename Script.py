25220108fe339d34e7d46e871248e67c54441eb1
Ian L <ian@inmotionvr.com>
2023-04-23 12:48:18 +0200

    🔨Add python script

---
+++ Script.py
@@ -0,0 +1,30 @@
+import sys
+
+# Open the file for reading and writing
+file = open("list.txt", "r+")
+
+existing_contents = file.read().splitlines()
+
+while True:
+    # Prompt the user for input
+    print("Enter some items to add on to the shopping list (enter quit to Quit): ")
+    data = input()
+
+    # Check if the user wants to quit
+    if data == "quit":
+        print("Exiting program...")
+        break
+
+    
+    if data in existing_contents:
+        print("Is already in the shopping list, skipping input")
+    else:
+        # Write the user's input to the file
+        file.write(data + "\n")
+
+        existing_contents.append(data)
+
+        print(data + " added to list successfully!")
+
+# Close the file
+file.close()
